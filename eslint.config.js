054355538427f047c472c15f7534c5fe0df93d2a
Lucas Yang <yangchenshin77@gmail.com>
2023-12-25 14:13:37 +0800

    Install eslint

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import ycs77, { GLOB_TS, GLOB_VUE } from '@ycs77/eslint-config'
+
+export default ycs77(
+  {
+    vue: true,
+    typescript: true,
+  },
+  {
+    files: [GLOB_TS, GLOB_VUE],
+    rules: {
+      'no-console': 'warn',
+
+      'ts/ban-ts-comment': 'off',
+      'ts/no-invalid-this': 'off',
+      'ts/prefer-ts-expect-error': 'off',
+    },
+  },
+  {
+    files: [GLOB_VUE],
+    rules: {
+      'vue/multiline-html-element-content-newline': 'off',
+      'vue/no-template-shadow': 'off',
+    },
+  },
+)
